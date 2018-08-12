aea45ee179ceadb7cbc5698e87bce507d02b330f
Ryota Kayanuma <picosushi12+git@gmail.com>
2018-08-13 03:45:38 +0900

    Implement wallpaper setting

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 from tempfile import mkstemp
 from random import choice
 from binascii import a2b_base64
-import click
+import subprocess
 
+import click
 import requests
 
-IDLIST_LOC = "./IDLIST"
+IDLIST_LOC = "./IDLIST"    # ???
+WALLPAPER_CMD = ['feh', '--bg-fill']    # Command to set wallpaper.
 
 
 def fetch_image(image_id):
@@ -14,7 +16,7 @@ def fetch_image(image_id):
     resp = requests.get(image_url)
     result = resp.json()
     image_bin = a2b_base64(str(result['dataUri'].split(',')[1]))
-    fname = mkstemp('-earth.jpg')[1]
+    fname = mkstemp('-earth-{}.jpg'.format(image_id))[1]
     with open(fname, 'wb') as f:
         f.write(image_bin)
     return fname
@@ -28,7 +30,11 @@ def main(set_wallpaper):
     image_id = choice(image_ids).strip()
     file_name = fetch_image(image_id)
     if set_wallpaper:
-        print(file_name)
+        # check if wallpaper command exists
+        if len(subprocess.run(['whereis', WALLPAPER_CMD[0]], stdout=subprocess.PIPE).stdout.decode('utf8').split()
+               ) <= 1:
+            raise Exception("Need {} command to set wallpaper!".format(WALLPAPER_CMD[0]))
+        subprocess.run(WALLPAPER_CMD + [file_name])
 
 
 if __name__ == '__main__':
